d1f8613825d45eeb54e2759a5e81072d6ae654a8
IGORSHN <IGORSHN@rafael.co.il>
2021-11-15 14:38:04 +0200

    add Infra/RequestsApi class

---
+++ Infra/Requests_API.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+
+class RequestsApi:
+    def __init__(self, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+        self.session = requests.Session()
+
+    @staticmethod
+    def _observe_response(response):
+        print(f'response status code: {response.status_code}')
+        print(f'response json: {response.json()}')
+        return response.json()
+
+    def get(self, url, input_data):
+        return self._observe_response(self.session.get(self.base_url+url, params=input_data, headers=self.headers))
+
+    def post(self, url, input_data):
+        return self._observe_response(self.session.post(self.base_url+url, data=json.dumps(input_data), headers=self.headers))
+
+    def put(self, url, input_data):
+        return self._observe_response(self.session.put(self.base_url+url, data=json.dumps(input_data), headers=self.headers))
+
+    def delete(self, url):
+        return self._observe_response(self.session.delete(self.base_url+url, headers=self.headers))
+
---
+++ Logic/SongsServerLogic.py
@@ -0,0 +1,4 @@
+from Infra.Requests_API import RequestsApi
+
+session = RequestsApi.__init__("http://127.0.0.1:3002", {'Content-Type': 'application/json'})
+
