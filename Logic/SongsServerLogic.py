36756cc3137d49f5f0994abc83df2c0d8be6044d
IGORSHN <IGORSHN@rafael.co.il>
2021-11-18 12:59:11 +0200

    add some tests

--- Logic/SongsServerLogic.py
+++ Logic/SongsServerLogic.py
@@ -5,7 +5,7 @@ session = RequestsApi("http://127.0.0.1:3002", {'Content-Type': 'application/jso
 
 # /users logic
 def add_user(input_data):
-    session.post("/users/add_user", input_data)
+    return session.post("/users/add_user", input_data)
 
 
 def get_user(input_data):
@@ -13,52 +13,56 @@ def get_user(input_data):
 
 
 def add_playlist(input_data):
-    session.post("/users/add_playlist", input_data)
+    return session.post("/users/add_playlist", input_data)
 
 
 def get_playlist(input_data):
-    session.get("/users/get_playlist", input_data)
+    return session.get("/users/get_playlist", input_data)
 
 
 def add_friend(input_data):
-    session.put("/users/add_friend", input_data)
+    return session.put("/users/add_friend", input_data)
 
 
 def change_password(input_data):
-    session.put("/users/change_password", input_data)
+    return session.put("/users/change_password", input_data)
 
 
 # /songs logic
 def add_song(input_data):
-    session.post("/songs/add_song", input_data)
+    return session.post("/songs/add_song", input_data)
 
 
 def decrease_rating(input_data):
-    session.put("/songs/downvote", input_data)
+    return session.put("/songs/downvote", input_data)
 
 
 def increase_rating(input_data):
-    session.put("/songs/upvote", input_data)
+    return session.put("/songs/upvote", input_data)
 
 
 def get_song(input_data):
-    session.get("/songs/get_song", input_data)
+    return session.get("/songs/get_song", input_data)
 
 
 def get_ranked_songs(input_data):
-    session.get("/songs/ranked_songs", input_data)
+    return session.get("/songs/ranked_songs", input_data)
 
 
 # /playlist logic
 def add_song_to_playlist(input_data):
-    session.post("/playlists/add_song", input_data)
+    return session.post("/playlists/add_song", input_data)
 
 
 # /admin logic
 def delete_users():
-    session.delete("/admin/delete_all_users")
+    return session.delete("/admin/delete_all_users")
 
 
 def delete_songs():
-    session.delete("/admin/delete_all_songs")
+    return session.delete("/admin/delete_all_songs")
+
+
+def admin_add_users(input_data):
+    return session.post("/admin/set_users", input_data)
 
--- Tests/test_users_functionality.py
+++ Tests/test_users_functionality.py
@@ -1,4 +1,5 @@
 import Logic.SongsServerLogic as sl
+import Logic.response_parser as rp
 import pytest
 
 user1 = {
@@ -6,14 +7,44 @@ user1 = {
     "user_password": "12345"
 }
 
+user2 = {
+    "user_name": "test_user2",
+    "user_password": "12345"
+}
+
 get_user1 = {"user_name": "test_user1"}
 
+add_friend = {
+    "user_name": "test_user1",
+    "user_password": "12345",
+    "friend_name": "test_user2"
+}
 
-def test_add_new_user():
+
+@pytest.fixture()
+def pretest_actions():
     sl.delete_songs()
     sl.delete_users()
+
+
+def test_add_new_user(pretest_actions):
     sl.add_user(user1)
     resp = sl.get_user(get_user1)
     print(resp)
-    u = resp['data']['user_name']
-    assert u == "test_user1", f'expected: test_user1, actual: {u}'
+    actual_user = rp.get_username(resp)
+    assert actual_user == "test_user1", f'expected: test_user1, actual: {actual_user}'
+
+
+def test_add_existing_user(pretest_actions):
+    sl.add_user(user1)
+    resp = sl.add_user(user1)
+    print(resp)
+
+
+def test_add_friend(pretest_actions):
+    sl.add_user(user1)
+    sl.add_user(user2)
+    sl.add_friend(add_friend)
+    resp = sl.get_user(user1)
+    print(rp.get_friend(resp))
+
